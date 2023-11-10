88f3e7b098a182c5370c6af9cb14a9f50d8c0c20
kitanovicd <kitanovic.danilo@outlook.com>
2023-11-10 14:05:58 +0100

    feat: seam airdrop

---
+++ src/SeamAirdrop.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {MerkleProof} from "openzeppelin-contracts/utils/cryptography/MerkleProof.sol";
+import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {ISeamAirdrop} from "./interfaces/ISeamAirdrop.sol";
+
+/// @title SeamAirDrop
+/// @notice Contract for airdropping tokens to community members
+/// @dev For every new airdrop, a new contract should be deployed
+contract SeamAirdrop is ISeamAirdrop, Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable seam;
+    bytes32 public merkleRoot;
+
+    mapping(address => bool) public hasClaimed;
+
+    constructor(IERC20 _seam, address _owner) Ownable(_owner) {
+        seam = _seam;
+    }
+
+    /// @inheritdoc ISeamAirdrop
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        merkleRoot = _merkleRoot;
+        emit MerkleRootSet(_merkleRoot);
+    }
+
+    /// @inheritdoc ISeamAirdrop
+    function claim(address recipient, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (hasClaimed[recipient]) {
+            revert AlreadyClaimed(recipient);
+        }
+        if (!MerkleProof.verify(merkleProof, merkleRoot, keccak256(abi.encodePacked(recipient, amount)))) {
+            revert InvalidProof();
+        }
+
+        hasClaimed[recipient] = true;
+        seam.safeTransfer(recipient, amount);
+
+        emit Claim(recipient, amount);
+    }
+}
---
+++ src/interfaces/ISeamAirdrop.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+/// @title Seam Airdrop Interface
+/// @notice Interface for the Seam Airdrop contract
+interface ISeamAirdrop {
+    event Claim(address indexed recipient, uint256 amount);
+    event MerkleRootSet(bytes32 merkleRoot);
+
+    error AlreadyClaimed(address recipient);
+    error InvalidProof();
+
+    /// @notice Changes merkle root
+    /// @param _merkleRoot New merkle root
+    /// @custom:usage Should be used only in cases merkle root is not generated properly or some addresses need to be included/excluded
+    function setMerkleRoot(bytes32 _merkleRoot) external;
+
+    /// @notice Claims tokens for the recipient
+    /// @dev If proof is invalid or recipient already claimed tokens, reverts, otherwise tokens are sent to the recipient
+    /// @param recipient Address to claim tokens for
+    /// @param amount Amount of tokens to claim
+    /// @param merkleProof Merkle proof
+    function claim(address recipient, uint256 amount, bytes32[] calldata merkleProof) external;
+}
