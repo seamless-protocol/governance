9a8d404d020e3cbcaae7db7e970d56493b864c8c
kitanovicd <kitanovic.danilo@outlook.com>
2023-11-13 13:34:27 +0100

    feat: interfaces

--- src/airdrop/EscrowSeamAirdrop.sol
+++ src/airdrop/EscrowSeamAirdrop.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.20;
 
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {IEscrowSeamAirdrop} from "../interfaces/IEscrowSeamAirdrop.sol";
 import {IEscrowSeam} from "../interfaces/IEscrowSeam.sol";
 import {Airdrop} from "./Airdrop.sol";
 
 /// @title Escrow Seam Airdrop
 /// @notice Airdrop contract that deposits SEAM into the EscrowSeam contract
-contract EscrowSeamAirdrop is Airdrop {
+/// @notice New contract should be deployed for each airdrop
+contract EscrowSeamAirdrop is IEscrowSeamAirdrop, Airdrop {
     IEscrowSeam public immutable escrowSeam;
 
     constructor(IERC20 _seam, IEscrowSeam _escrowSeam, bytes32 _merkleRoot, address _owner)
--- src/airdrop/SeamAirdrop.sol
+++ src/airdrop/SeamAirdrop.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.20;
 
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {ISeamAirdrop} from "../interfaces/ISeamAirdrop.sol";
 import {Airdrop} from "./Airdrop.sol";
 
 /// @title Seam Airdrop
 /// @notice Airdrop contract that transfers SEAM tokens
-contract SeamAirdrop is Airdrop {
+/// @dev New contract should be deployed for each airdrop
+contract SeamAirdrop is ISeamAirdrop, Airdrop {
     constructor(IERC20 _seam, bytes32 _merkleRoot, address _owner) Airdrop(_seam, _merkleRoot, _owner) {}
 
     /// @inheritdoc Airdrop
