4a231662157c2e09d6dd632ed8014e54c7b86e7c
kitanovicd <kitanovic.danilo@outlook.com>
2023-12-05 12:21:59 +0100

    feat: new coding style for escrow seam contract

--- src/interfaces/IEscrowSeam.sol
+++ src/interfaces/IEscrowSeam.sol
@@ -3,35 +3,44 @@ pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/// @notice Interface for EscrowSeam contract.
 interface IEscrowSeam is IERC20 {
-    struct VestingData {
-        uint256 claimableAmount;
-        uint256 vestPerSecond;
-        uint256 vestingEndsAt;
-        uint256 lastUpdatedTimestamp;
-    }
-
-    struct EscrowSeamStorage {
-        IERC20 seam;
-        uint256 vestingDuration;
-        mapping(address => VestingData) vestingInfo;
-    }
-
     error NonTransferable();
     error ZeroAmount();
 
     event Deposit(address indexed from, address indexed onBehalfOf, uint256 amount);
     event Claim(address indexed user, uint256 amount);
 
-    function seam() external view returns (address);
+    /// @notice Returns the SEAM token address.
+    /// @return seamAddress SEAM token address
+    function seam() external view returns (address seamAddress);
 
-    function vestingDuration() external view returns (uint256);
+    /// @notice Returns the vesting duration.
+    /// @return duration Vesting duration
+    function vestingDuration() external view returns (uint256 duration);
 
-    function vestingInfo(address) external view returns (uint256, uint256, uint256, uint256);
+    /// @notice Returns the vesting info for the given account.
+    /// @param account Account to query
+    /// @return claimableAmount Claimable amount
+    /// @return vestPerSecond Amount of vested tokens per second
+    /// @return vestingEndsAt Timestamp when vesting ends
+    /// @return lastUpdatedTimestamp Last updated timestamp
+    function vestingInfo(address account)
+        external
+        view
+        returns (uint256 claimableAmount, uint256 vestPerSecond, uint256 vestingEndsAt, uint256 lastUpdatedTimestamp);
 
-    function getClaimableAmount(address) external view returns (uint256);
+    /// @notice Calculates and returns total claimable(vested) amount of the account at the moment.
+    /// @param account Account to query claimable amount for
+    /// @return amount Claimable amount
+    function getClaimableAmount(address account) external view returns (uint256 amount);
 
-    function deposit(address, uint256) external;
+    /// @notice Vests SEAM token for user.
+    /// @param onBehalfOf Account to vest SEAM token for
+    /// @param amount Amount to vest
+    function deposit(address onBehalfOf, uint256 amount) external;
 
-    function claim(address) external;
+    /// @notice Claims vested SEAM token for user.
+    /// @param account Account to claim SEAM token for
+    function claim(address account) external;
 }
---
+++ src/storage/EscrowSeamStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+library EscrowSeamStorage {
+    struct VestingData {
+        uint256 claimableAmount;
+        uint256 vestPerSecond;
+        uint256 vestingEndsAt;
+        uint256 lastUpdatedTimestamp;
+    }
+
+    struct Layout {
+        IERC20 seam;
+        uint256 vestingDuration;
+        mapping(address => VestingData) vestingInfo;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("seamless.contracts.storage.EscrowSeam")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant STORAGE_SLOT = 0x6393c68bbda65a43373480543c4f1ff15eb61969ce223f59d8fd1889e26cc300;
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
