2cbc793ec24e0c91aa1728c1b817a14012b839ab
kitanovicd <kitanovic.danilo@outlook.com>
2024-05-14 21:29:14 +0200

    fix: update slot

--- src/storage/StakedTokenStorage.sol
+++ src/storage/StakedTokenStorage.sol
@@ -39,8 +39,7 @@ library StakedTokenStorage {
         mapping(address => mapping(address => uint256)) accruedRewards;
     }
 
-    // TODO: Update this slot and comment
-    // keccak256(abi.encode(uint256(keccak256("seamless.contracts.storage.EscrowSeam")) - 1)) & ~bytes32(uint256(0xff))
+    // keccak256(abi.encode(uint256(keccak256("seamless.contracts.storage.StakedToken")) - 1)) & ~bytes32(uint256(0xff))
     bytes32 private constant STORAGE_SLOT = 0x6393c68bbda65a43373480543c4f1ff15eb61969ce223f59d8fd1889e26cc300;
 
     function layout() internal pure returns (Layout storage l) {
