da2611ab445cc5cb32fca50e2c492f81a7cb78a0
jcislo <pleja918@gmail.com>
2025-01-02 23:31:59 -0500

    stakedToken compiles

--- src/safetyModule/stakedToken.sol
+++ src/safetyModule/stakedToken.sol
@@ -4,12 +4,22 @@ pragma solidity ^0.8.20;
 import {Initializable} from "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
 import {AccessControlUpgradeable} from "openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {VotesUpgradeable} from "openzeppelin-contracts-upgradeable/governance/utils/VotesUpgradeable.sol";
+import {Checkpoints} from "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
+import {IRewardsController} from "./interfaces/IRewardsController.sol";
 import {ERC4626Upgradeable} from "openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
-import {PausableUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/utils/PausableUpgradeable.sol";
 
-contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable, ERC4626Upgradeable, PausableUpgradeable {
+import {PausableUpgradeable} from "openzeppelin-contracts-upgradeable/utils/PausableUpgradeable.sol";
+
+contract StakedToken is 
+    UUPSUpgradeable, 
+    AccessControlUpgradeable, 
+    ERC4626Upgradeable, 
+    PausableUpgradeable,
+    VotesUpgradeable 
+{
     using SafeERC20 for IERC20;
 
     // errors
@@ -17,11 +27,12 @@ contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable
     error isZeroAddress(address target);
     error InsufficientStake();
     error CooldownActive();
+    error ERC20ExceededSafeSupply(uint256 increasedSupply, uint256 cap);
 
     // events
     event EmergencyActive(bool status);
     event EmergencyWithdraw(address to, uint256 amt);
-    event RewardControllerChange(address RewardController);
+    event RewardsControllerChange(address RewardsController);
     event Cooldown(address user);
 
     // Global State
@@ -30,11 +41,9 @@ contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable
     /// @dev role which can upgrade the contract
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
-    IERC20 asset;
-
     bool isEmergencyWithdrawal;
 
-    address RewardController;
+    IRewardsController rewardsController;
 
     /// @notice Seconds to wait before unstake window is available
     uint256 public COOLDOWN_SECONDS;
@@ -57,27 +66,29 @@ contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable
     }
 
     /// @notice Initializes the token storage and inherited contracts.
-    /// @param seam SEAM token address
-    /// @param emissionPerSecond Emission per second
+    /// @param _asset token address of the asset
+    /// @param controller address of the rewards controller
     /// @param initialAdmin Initial admin of the contract
+    /// @param _erc20name name of the share token
+    /// @param _erc20symbol symbol of the share token
     function initialize(
         address _asset,
         address controller,
         address initialAdmin,
-        string _erc20name, 
-        string _erc20symbol
+        string calldata _erc20name, 
+        string calldata _erc20symbol
     ) external initializer {
         __AccessControl_init();
         __UUPSUpgradeable_init();
-        __ERC4626_init(_asset);
         __ERC20_init(_erc20name, _erc20symbol);
+        __ERC4626_init(IERC20(_asset));
+        
         __Pausable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, initialAdmin);
         _grantRole(MANAGER_ROLE, initialAdmin);
        
-        asset = IERC20(_asset);
-        RewardController = controller;
+        rewardsController = IRewardsController(controller);
     }
 
     /// @inheritdoc UUPSUpgradeable
@@ -101,7 +112,7 @@ contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable
     }
 
     function emergencyWithdrawal(address to, uint256 amt) external onlyRole(MANAGER_ROLE) {
-        bool success = asset.transfer(to, amt);
+        bool success = IERC20(asset()).transfer(to, amt);
         if (!success) revert SendFailed();
         emit EmergencyWithdraw(to, amt);
     }
@@ -112,7 +123,7 @@ contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable
      * @dev Begins the cooldown period to unstake
      * @notice Requires an active stake to activate
      **/
-    function cooldown() external override {
+    function cooldown() external {
         if (balanceOf(msg.sender) == 0) revert InsufficientStake();
         stakersCooldowns[msg.sender] = block.timestamp;
 
@@ -178,7 +189,7 @@ contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable
         super._withdraw(caller, receiver, owner, assets, shares);
     }
 
-    function _checkCooldown(uint256 cd) internal returns(bool) {
+    function _checkCooldown(uint256 cd) internal view returns(bool) {
         uint256 cooldownEnd = cd + COOLDOWN_SECONDS;
         if (block.timestamp > cooldownEnd) {
             return false;
@@ -211,14 +222,24 @@ contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable
         // Recipient
         if (from != to) {
             uint256 previousSenderCooldown = stakersCooldowns[from];
-            stakersCooldowns[to] = getNextCooldownTimestamp(previousSenderCooldown, amount, to, balTo);
+            stakersCooldowns[to] = getNextCooldownTimestamp(previousSenderCooldown, value, to, balTo);
             // if cooldown was set and whole balance of sender was transferred - clear cooldown
-            if (balFrom == amount && previousSenderCooldown != 0) {
+            if (balFrom == value && previousSenderCooldown != 0) {
                 stakersCooldowns[from] = 0;
             }
         }
 
         super._update(from, to, value);
+
+        //ERC20Votes
+        if (from == address(0)) {
+            uint256 supply = totalSupply();
+            uint256 cap = _maxSupply();
+            if (supply > cap) {
+                revert ERC20ExceededSafeSupply(supply, cap);
+            }
+        }
+        _transferVotingUnits(from, to, value);
     }
 
     function _handleAction(
@@ -226,22 +247,56 @@ contract StakedToken is UUPSUpgradeable, AccessControlUpgradeable, Initializable
         uint256 totalSupply,
         uint256 oldUserBalance
     ) internal {
-        IPoolAddressesProvider poolAddressProvider =
-            Storage.layout().poolAddressProvider;
+        
 
-        IRewardsController rewardsController = IRewardsController(
-            poolAddressProvider.getAddress(INCENTIVES_CONTROLLER)
-        );
-
-        if (address(rewardsController) != address(0)) {
-            rewardsController.handleAction(user, totalSupply, oldUserBalance);
-        }
+        rewardsController.handleAction(user, totalSupply, oldUserBalance);
+        
     }
 
     // Admin Functions
     function changeController(address newController) external isNotZeroAddress(newController) onlyRole(MANAGER_ROLE) {
-        RewardController = newController;
-        emit RewardControllerChange(newController);
+        rewardsController = IRewardsController(newController);
+        emit RewardsControllerChange(newController);
+    }
+
+    // ERC20Votes
+
+    /**
+     * @dev Maximum token supply. Defaults to `type(uint208).max` (2^208^ - 1).
+     *
+     * This maximum is enforced in {_update}. It limits the total supply of the token, which is otherwise a uint256,
+     * so that checkpoints can be stored in the Trace208 structure used by {{Votes}}. Increasing this value will not
+     * remove the underlying limitation, and will cause {_update} to fail because of a math overflow in
+     * {_transferVotingUnits}. An override could be used to further restrict the total supply (to a lower value) if
+     * additional logic requires it. When resolving override conflicts on this function, the minimum should be
+     * returned.
+     */
+    function _maxSupply() internal view virtual returns (uint256) {
+        return type(uint208).max;
+    }
+
+    /**
+     * @dev Returns the voting units of an `account`.
+     *
+     * WARNING: Overriding this function may compromise the internal vote accounting.
+     * `ERC20Votes` assumes tokens map to voting units 1:1 and this is not easy to change.
+     */
+    function _getVotingUnits(address account) internal view virtual override returns (uint256) {
+        return balanceOf(account);
+    }
+
+    /**
+     * @dev Get number of checkpoints for `account`.
+     */
+    function numCheckpoints(address account) public view virtual returns (uint32) {
+        return _numCheckpoints(account);
+    }
+
+    /**
+     * @dev Get the `pos`-th checkpoint for `account`.
+     */
+    function checkpoints(address account, uint32 pos) public view virtual returns (Checkpoints.Checkpoint208 memory) {
+        return _checkpoints(account, pos);
     }
 
 }
