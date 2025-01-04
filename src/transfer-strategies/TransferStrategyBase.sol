f21c4f2053ccf815f8428e0b2cea85bcae64148f
jcislo <pleja918@gmail.com>
2025-01-04 03:44:51 -0500

    rm oz from node_modules

--- src/transfer-strategies/TransferStrategyBase.sol
+++ src/transfer-strategies/TransferStrategyBase.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {ITransferStrategyBase} from "../interfaces/ITransferStrategyBase.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 
 /// @dev Base contract for transfer strategies updated from this source
 ///      https://github.com/aave/aave-v3-periphery/blob/master/contracts/rewards/transfer-strategies/TransferStrategyBase.sol
