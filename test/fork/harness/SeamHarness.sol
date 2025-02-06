a6f8b945d07bb3fe45cbc890f5fafa9bb52fed6d
fredwes <6827305+fredwes@users.noreply.github.com>
2025-02-06 10:47:49 -0800

    chore: comment

--- test/fork/harness/SeamHarness.sol
+++ test/fork/harness/SeamHarness.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Seam} from "../../../src/Seam.sol";
 
-/// @notice Wrapper contract that exposes all internal functions ofFeeManager
+/// @notice Wrapper contract that exposes internal functions Seam
 contract SeamHarness is Seam {
     function exposed_mint(address account, uint256 value) external {
         _mint(account, value);
